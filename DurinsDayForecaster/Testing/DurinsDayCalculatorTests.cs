6cb045c843a8682a46d51886856ee5e989231d38
Jeff Linton <jslinton44@gmail.com>
2016-02-11 08:38:53 -0700

    Adds unit testing

---
+++ DurinsDayForecaster/Testing/DurinsDayCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using DurinsDayForecaster.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Testing
+{
+
+    public class DurinsDayCalculatorTests
+    {
+        [Fact]
+        public void In_2014_durins_day_should_be_october_23rd()
+        {
+            var durinsDay = new DurinsDayCalculator().DurinsDay(2014);
+
+            Assert.Equal(durinsDay, new DateTime(2014, 10, 23));
+        }
+    }
+}
